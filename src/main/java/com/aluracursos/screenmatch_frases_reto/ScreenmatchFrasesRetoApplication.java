2a0d851edd99f7ee01267e157a32ea20535a7f55
Daniel Romero <dandres508@gmail.com>
2024-11-13 13:40:37 -0500

    conexion a la base de datos

--- src/main/java/com/aluracursos/screenmatch_frases_reto/ScreenmatchFrasesRetoApplication.java
+++ src/main/java/com/aluracursos/screenmatch_frases_reto/ScreenmatchFrasesRetoApplication.java
@@ -1,15 +1,19 @@
 package com.aluracursos.screenmatch_frases_reto;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ScreenmatchFrasesRetoApplication {
 
+    public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 
 
-	public static void main(String[] args) {
-		SpringApplication.run(ScreenmatchFrasesRetoApplication.class, args);
-	}
+        SpringApplication.run(ScreenmatchFrasesRetoApplication.class, args);
+    }
 
 }
